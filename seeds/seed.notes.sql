3c637ae72017f2464835d94927126f7be024a6f1
Leah White <leahawhite@gmail.com>
2019-09-04 19:51:44 -0700

    db migrations seeds done

---
+++ seeds/seed.notes.sql
@@ -0,0 +1,6 @@
+INSERT INTO notes (what, how, who, link, thoughts, favorite, author, folder)
+VALUES
+('Los Espookys', 'HBO', 'Me', 'https://www.hbo.com/los-espookys', 'Julio Torres! Looks promising, but the subtitles were quick!', true, 1, 1),
+('On Becoming a God in Central Florida', 'Showtime', 'Me', 'https://www.sho.com/on-becoming-a-god-in-central-florida', 'Kirsten Dunst pyramid scheme series', true, 1, 1),
+('Fosse/Verdon', 'Hulu', 'Me', 'https://www.fxnetworks.com/shows/fosse-verdon', 'Bob Fosse/Gwen Verdon series with Sam Rockwell and Michelle Williams.', false, 1, 1),
+('Schitt''s Creek', 'Netflix', 'Emily', null, 'She''s asked me twice if I have seen this. I should?', true, 1, 1);
---
+++ seeds/seed.users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (full_name, email, password)
+VALUES
+('Demo User', 'demo@demo.com', 'demo123');
