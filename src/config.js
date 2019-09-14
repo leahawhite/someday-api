71c6bf180c128a6de12883ec4400eddcd5a7d63a
Leah White <leahawhite@gmail.com>
2019-09-14 13:33:33 -0700

    heroku auth

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   DB_URL: process.env.DATABASE_URL,
   TEST_DB_URL: process.env.TEST_DB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/api',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api' 
 }
