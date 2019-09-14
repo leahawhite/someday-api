95ccffadd89b0fc19b91e4bcdf8078d9a578b5cb
Leah White <leahawhite@gmail.com>
2019-09-14 00:23:01 -0700

    predeploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL,
-  JWT_SECRET: process.env.JWT_SECRET
+  DB_URL: process.env.DATABASE_URL,
+  TEST_DB_URL: process.env.TEST_DB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/api',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api' 
 }
